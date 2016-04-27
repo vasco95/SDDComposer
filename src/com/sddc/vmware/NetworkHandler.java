6f66f1d42ff2364ac2a9a59dcffa9f7751a0c273
aadeshmagare <aadeshmagare01@gmail.com>
2016-04-27 17:44:18 +0530

    Adding Network Switch to host

--- src/com/sddc/vmware/NetworkHandler.java
+++ src/com/sddc/vmware/NetworkHandler.java
@@ -6,8 +6,11 @@ import org.slf4j.LoggerFactory;
 import com.vmware.vim25.AlreadyExistsFaultMsg;
 import com.vmware.vim25.HostConfigFaultFaultMsg;
 import com.vmware.vim25.HostConfigManager;
+import com.vmware.vim25.HostNetworkPolicy;
+import com.vmware.vim25.HostPortGroupSpec;
 import com.vmware.vim25.HostVirtualSwitchSpec;
 import com.vmware.vim25.ManagedObjectReference;
+import com.vmware.vim25.NotFoundFaultMsg;
 import com.vmware.vim25.ResourceInUseFaultMsg;
 import com.vmware.vim25.RuntimeFaultFaultMsg;
 import com.vmware.vim25.ServiceContent;
@@ -18,12 +21,13 @@ public class NetworkHandler {
 	private VimPortType vimPort;
 	private ServiceContent serviceContent;
 	private static Logger logger = LoggerFactory.getLogger(NetworkHandler.class);
+
 	public NetworkHandler(VimPortType vimPort, ServiceContent serviceContent) {
 		this.vimPort = vimPort;
 		this.serviceContent = serviceContent;
 		this.mSelector = new MOREFSelector(vimPort, serviceContent);
 	}
-	
+
 	public void createVirtualSwitch(String switchName, String hostName) {
 		ManagedObjectReference rootFolder = this.serviceContent.getRootFolder();
 		//Get Hostsystem MOR
@@ -32,13 +36,41 @@ public class NetworkHandler {
 			logger.error("No host exists of name " + hostName);
 			return;
 		}
-		HostConfigManager hostCfgMgr = (HostConfigManager) this.mSelector.getEntityProps(hostMor, new String[] {"configManager"});
+		HostConfigManager hostCfgMgr = (HostConfigManager) this.mSelector.getEntityProps(hostMor, new String[] {"configManager"}).get("configManager");
 		ManagedObjectReference nwSystem = hostCfgMgr.getNetworkSystem();
         HostVirtualSwitchSpec spec = new HostVirtualSwitchSpec();
         spec.setNumPorts(8);
         try {
 			vimPort.addVirtualSwitch(nwSystem, switchName, spec);
 		} catch (AlreadyExistsFaultMsg | HostConfigFaultFaultMsg | ResourceInUseFaultMsg | RuntimeFaultFaultMsg e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void createVirtualPortGroup(String switchName, String hostName, String portGroupName) {
+		ManagedObjectReference rootFolder = this.serviceContent.getRootFolder();
+		//Get Hostsystem MOR
+		ManagedObjectReference hostMor = this.mSelector.getMOREFByName(rootFolder, "HostSystem", hostName);
+		if(hostMor == null) {
+			logger.error("No host exists of name " + hostName);
+			return;
+		}
+		HostConfigManager hostCfgMgr = (HostConfigManager) this.mSelector.getEntityProps(hostMor, new String[] {"configManager"}).get("configManager");
+		ManagedObjectReference nwSystem = hostCfgMgr.getNetworkSystem();
+		HostPortGroupSpec portgrp = new HostPortGroupSpec();
+		portgrp.setName(portGroupName);
+		portgrp.setVswitchName(switchName);
+		portgrp.setPolicy(new HostNetworkPolicy());
+
+    	try {
+			vimPort.addPortGroup(nwSystem, portgrp);
+		} catch (AlreadyExistsFaultMsg e) {
+			e.printStackTrace();
+		} catch (HostConfigFaultFaultMsg e) {
+			e.printStackTrace();
+		} catch (NotFoundFaultMsg e) {
+			e.printStackTrace();
+		} catch (RuntimeFaultFaultMsg e) {
 			e.printStackTrace();
 		}
 	}
